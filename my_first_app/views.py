af9d9655684d38d3cd697352f47a70f430d59336
Realmarvin17 <zverevdm@mail.ru>
2024-02-01 14:19:47 +0300

    add homework seminar 1

---
+++ my_first_app/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from datetime import datetime
+
+
+def home_view(request):
+    html = f"""
+    <html>
+    <body>
+        <h1>Welcome to my first Django site!</h1>
+        <p>Today is {datetime.now().strftime('%B %d, %Y')}.</p>
+        <p>This is my home page.</p>
+    </body>
+    </html>
+    """
+    # Log visit data
+    with open('visits.log', 'a') as f:
+        f.write(f"{datetime.now()} - Home page visited.\n")
+
+    return HttpResponse(html)
+
+
+def about_view(request):
+    html = f"""
+    <html>
+    <body>
+        <h1>About me</h1>
+        <p>This is a page about myself.</p>
+        <p>I am learning Django.</p>
+    </body>
+    </html>
+    """
+    # Log visit data
+    with open('visits.log', 'a') as f:
+        f.write(f"{datetime.now()} - About page visited.\n")
+
+    return HttpResponse(html)
